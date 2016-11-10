01315bb891e9375919f62dccbe47f714d83b6a18
Fadi Khadra <fdkhadra@gmail.com>
2016-11-10 09:34:11 +0100

    Added top-center and bottom-center positionning

--- src/config.js
+++ src/config.js
@@ -3,8 +3,10 @@ export default {
   POSITION: {
     TOP_LEFT: 'top-left',
     TOP_RIGHT: 'top-right',
+    TOP_CENTER: 'top-center',
     BOTTOM_LEFT: 'bottom-left',
-    BOTTOM_RIGHT: 'bottom-right'
+    BOTTOM_RIGHT: 'bottom-right',
+    BOTTOM_CENTER: 'bottom-center'
   },
   TYPE: {
     INFO: 'info',
