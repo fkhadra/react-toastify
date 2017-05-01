1cbcae18cd199dd9d3964db6113cfd7e15bf05b5
Fadi Khadra <fdkhadra@gmail.com>
2017-05-01 21:37:25 +0200

    Simplify object.values

--- src/util/objectValues.js
+++ src/util/objectValues.js
@@ -1,9 +1,5 @@
 export default function (obj) {
   const values = [];
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      values.push(obj[key]);
-    }
-  }
+  Object.keys(obj).forEach(key => values.push(obj[key]));
   return values;
 }
