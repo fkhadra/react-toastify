94ef1fce9cc19c9587c6f5c9da016c5bf56faea5
Fadi Khadra <fdkhadra@gmail.com>
2017-05-12 17:19:06 +0200

    Removed renderer, not used at all

--- src/__tests__/ToastContainer.js
+++ src/__tests__/ToastContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import renderer from 'react-test-renderer';
 
 import ToastContainer from './../ToastContainer';
 import Toast from './../Toast';
