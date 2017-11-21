edcfd93b01259d3db080b72f30ab0f0d6c51f78b
Fadi Khadra <fdkhadra@gmail.com>
2017-11-21 14:16:35 +0100

    Move sass variables to js file

---
+++ src/config.js
@@ -0,0 +1,14 @@
+export default {
+  width: "320px",
+  background: "#ffffff",
+  fontColor: "#999",
+  fontSize: "13px",
+  animationDuration: "0.75s",
+  colorDefault: "#fff",
+  colorInfo: "#3498db",
+  colorSuccess: "#07bc0c",
+  colorWarning: "#f1c40f",
+  colorError: "#e74c3c",
+  colorProgressDefault: "linear-gradient(to right, #4cd964, #5ac8fa, #007aff, #34aadc, #5856d6, #ff2d55)",
+  smartphonePortrait: "only screen and (max-width : 480px)",
+};
