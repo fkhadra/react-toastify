f1efd8caca7baca4af5ef4abf7cd7a3d12777baf
Fadi Khadra <fdkhadra@gmail.com>
2018-01-23 12:17:53 +0100

    Fix test

--- src/__tests__/ToastContainer.js
+++ src/__tests__/ToastContainer.js
@@ -131,6 +131,7 @@ describe('ToastContainer', () => {
 
     const props = getToastProps(component);
     expect(Object.keys(props.closeButton.props)).toMatchObject([
+      'ariaLabel',
       'closeToast',
       'type'
     ]);
