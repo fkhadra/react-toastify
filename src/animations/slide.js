0b6e16d74519434134df841325524a0f9dccf901
Fadi Khadra <fdkhadra@gmail.com>
2018-03-16 09:46:16 +0100

    Add slide animation and move default to bounce

---
+++ src/animations/slide.js
@@ -0,0 +1,80 @@
+import { POSITION } from './../constant';
+
+// animation state used by enter:to and exit:from
+const commonState = { transform: 'translate3d(0, 0, 0)' };
+
+export default function getAnimation(pos) {
+  switch (pos) {
+    case POSITION.TOP_RIGHT:
+    case POSITION.BOTTOM_RIGHT:
+    default:
+      return {
+        enter: {
+          from: {
+            transform: 'translate3d(-100%, 0, 0)',
+            visibility: 'visible'
+          },
+          to: commonState
+        },
+        exit: {
+          from: commonState,
+          to: {
+            visibility: 'hidden',
+            transform: 'translate3d(100%, 0, 0)'
+          }
+        }
+      };
+    case POSITION.TOP_LEFT:
+    case POSITION.BOTTOM_LEFT:
+      return {
+        enter: {
+          from: {
+            transform: 'translate3d(100%, 0, 0)',
+            visibility: 'visible'
+          },
+          to: commonState
+        },
+        exit: {
+          from: commonState,
+          to: {
+            visibility: 'hidden',
+            transform: 'translate3d(100%, 0, 0)'
+          }
+        }
+      };
+    case POSITION.BOTTOM_CENTER:
+      return {
+        enter: {
+          from: {
+            transform: 'translate3d(0, 100%, 0)',
+            visibility: 'visible'
+          },
+          to: commonState
+        },
+        exit: {
+          from: commonState,
+          to: {
+            visibility: 'hidden',
+            transform: 'translate3d(0, 100%, 0)'
+          }
+        }
+      };
+    case POSITION.TOP_CENTER:
+      return {
+        enter: {
+          from: {
+            transform: 'translate3d(0, -100%, 0)',
+            visibility: 'visible'
+          },
+          to: commonState
+        },
+        exit: {
+          from: commonState,
+          to: {
+            visibility: 'hidden',
+            transform: 'translate3d(0, -100%, 0)'
+          }
+        }
+      };
+  }
+}
