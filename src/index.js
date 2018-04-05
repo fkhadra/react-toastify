7c3fabe2142b1f8abf9999bc0c09d047d7f22cd0
Fadi Khadra <fdkhadra@gmail.com>
2018-04-05 22:16:11 +0200

    Export transitions and helper

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import ToastContainer from './components/ToastContainer';
+import { Bounce, Slide, Zoom, Flip } from './components/Transitions';
 import toaster from './toaster';
+import cssTransition from './utils/cssTransition';
 
-export { ToastContainer, toaster as toast };
+export {
+  ToastContainer,
+  toaster as toast,
+  cssTransition,
+  Bounce,
+  Slide,
+  Zoom,
+  Flip
+};
