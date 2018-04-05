481aeaa84b7f91c6ad5133d5f98a463d33b3b72e
Fadi Khadra <fdkhadra@gmail.com>
2018-04-05 22:00:56 +0200

    Remove defaultStyle helper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import ToastContainer from './components/ToastContainer';
 import toaster from './toaster';
-import { defineStyle } from './utils/defaultStyle';
 
-export { ToastContainer, toaster as toast, defineStyle as style };
+export { ToastContainer, toaster as toast };
