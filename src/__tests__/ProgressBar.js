fb9a31320043d2757abaec72199fc8cdf6646ddb
Fadi Khadra <fdkhadra@gmail.com>
2018-05-02 15:45:36 +0200

    Try to clean the mess

--- src/__tests__/ProgressBar.js
+++ src/__tests__/ProgressBar.js
@@ -1,7 +1,6 @@
 /* eslint-env jest */
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 
 import ProgressBar from './../components/ProgressBar';
 
