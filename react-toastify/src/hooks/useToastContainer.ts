c53c5a08f736c37a2c60c657546d1173aedb0d0e
Fadi <fdkhadra@gmail.com>
2020-02-03 21:52:53 +0100

    ensure we add closeButton only on component

--- react-toastify/src/hooks/useToastContainer.ts
+++ react-toastify/src/hooks/useToastContainer.ts
@@ -184,7 +184,7 @@ export function useToastContainer(props: ToastContainerProps) {
 
     let toastContent = content;
 
-    if (isValidElement(content)) {
+    if (isValidElement(content) && !isStr(content.type)) {
       toastContent = cloneElement(content, {
         closeToast
       });
