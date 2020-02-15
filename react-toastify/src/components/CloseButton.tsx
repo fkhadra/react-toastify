419d7dc855e318a49da86b51d8dae26d3d1363c4
Fadi <fdkhadra@gmail.com>
2020-02-15 14:35:58 +0100

    remove unecessary tag

--- react-toastify/src/components/CloseButton.tsx
+++ react-toastify/src/components/CloseButton.tsx
@@ -32,7 +32,7 @@ export function CloseButton({ closeToast, type, ariaLabel }: CloseButtonProps) {
         <path
           fillRule="evenodd"
           d="M7.71 8.23l3.75 3.75-1.48 1.48-3.75-3.75-3.75 3.75L1 11.98l3.75-3.75L1 4.48 2.48 3l3.75 3.75L9.98 3l1.48 1.48-3.75 3.75z"
-        ></path>
+        />
       </svg>
     </button>
   );
