e923eed477016c5c39228e7c56501b4bbfedce1d
Fadi <fdkhadra@gmail.com>
2020-02-17 14:27:26 +0100

    add test for cssTransition

--- test/__mocks__/react-transition-group.tsx
+++ test/__mocks__/react-transition-group.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 
 jest.mock('react-transition-group', () => {
   // @ts-ignore ...rest
-  const FakeTransition = jest.fn(({ children, className, style, ...rest }) => {
-    const props: Record<string, any> = {};
-    
-    if (className) props.className = className;
-    if (style) props.style = style;
+  const FakeTransition = jest.fn(
+    ({ children, className, style, onEnter, onEntered, onExit }) => {
+      const props: Record<string, any> = {};
+      const node = document.createElement('div');
 
-    return (
-      <div {...props}>
-        {children}
-      </div>
-    );
-  });
+      if (className) props.className = className;
+      if (style) props.style = style;
+
+      if (onEnter)
+        typeof onEnter === 'function' && onEnter(node);
+      if (onEntered)
+        typeof onEntered === 'function' &&
+          onEntered(node);
+      if (onExit)
+        typeof onExit === 'function' && onExit(node);
+
+      return <div {...props}>{children}</div>;
+    }
+  );
 
   return {
     Transition: FakeTransition,
--- test/utils/cssTransition.test.tsx
+++ test/utils/cssTransition.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-// import '../__mocks__/react-transition-group';
+import '../__mocks__/react-transition-group';
 import { cssTransition } from '../../src/utils';
 
+// TODO: write better tests 😜
 describe('cssTransition helper', () => {
   it('Should return a valid react component', () => {
     const Transition = cssTransition({
@@ -18,6 +19,40 @@ describe('cssTransition helper', () => {
       </Transition>
     );
 
+    expect(getByText('Plop').textContent).toContain('Plop');
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Should be possible to specify different duration for enter and exit', () => {
+    const Transition = cssTransition({
+      enter: 'foo',
+      exit: 'bar',
+      duration: [300, 500]
+    });
+
+    const Child = () => <div>Plop</div>;
+    const { container, getByText } = render(
+      <Transition>
+        <Child />
+      </Transition>
+    );
+    expect(getByText('Plop').textContent).toContain('Plop');
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Should be possible to prevent exit transition', () => {
+    const Transition = cssTransition({
+      enter: 'foo',
+      exit: 'bar',
+      duration: [300, 500]
+    });
+
+    const Child = () => <div>Plop</div>;
+    const { container, getByText } = render(
+      <Transition preventExitTransition>
+        <Child />
+      </Transition>
+    );
     expect(getByText('Plop').textContent).toContain('Plop');
     expect(container).toMatchSnapshot();
   });
