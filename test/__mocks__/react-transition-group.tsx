62628647ce283cc53bd2a50abfac02b8724f404f
Fadi <fdkhadra@gmail.com>
2020-05-09 21:20:57 +0200

    rework transition mock

--- test/__mocks__/react-transition-group.tsx
+++ test/__mocks__/react-transition-group.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
+import { isFn } from '../../src/utils';
 
 jest.mock('react-transition-group', () => {
-  // @ts-ignore ...rest
   const FakeTransition = jest.fn(
-    ({ children, className, style, onEnter, onEntered, onExit }) => {
-      const props: Record<string, any> = {};
-      const node = document.createElement('div');
+    ({ children, onEnter, onEntered, onExit, nodeRef, in: isIn }) => {
+      if (onEnter) isFn(onEnter) && isIn && onEnter(nodeRef);
+      if (onEntered) isFn(onEntered) && isIn && onEntered(nodeRef);
+      if (onExit) isFn(onExit) && !isIn && onExit(nodeRef);
 
-      if (className) props.className = className;
-      if (style) props.style = style;
-
-      if (onEnter)
-        typeof onEnter === 'function' && onEnter(node);
-      if (onEntered)
-        typeof onEntered === 'function' &&
-          onEntered(node);
-      if (onExit)
-        typeof onExit === 'function' && onExit(node);
-
-      return <div {...props}>{children}</div>;
+      return isIn ? <div>{children}</div> : null;
     }
   );
 
   return {
     Transition: FakeTransition,
-    TransitionGroup: FakeTransition
+    TransitionGroup: jest.fn(({ children }) => children)
   };
 });
