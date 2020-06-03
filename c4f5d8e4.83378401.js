7991156b257c8c61c0e02a3829411ab0c74fba7f
Fadi <fdkhadra@gmail.com>
2020-06-03 16:43:03 +0200

    Deploy website version based on 0d1600dac13da9a2f3c6f29ef45ad82ea5134915

---
+++ c4f5d8e4.83378401.js
@@ -0,0 +1,18 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{134:function(t,e,n){"use strict";n.r(e);var r=n(0),a=n.n(r),c=n(148),i=(n(156),n(149));const o=c.a.header`
+  background: black;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`,u=c.a.p`
+  margin: 0;
+  padding: 0 20px;
+  font-size: 1.5em;
+  color: #e7dacb;
+`,s=c.a.a`
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+`;e.default=function(){return a.a.createElement(o,null,a.a.createElement(u,null,"Black lives matter.",a.a.createElement("a",{href:"https://support.eji.org/give/153413/#!/donation/checkout",target:"_blank"},"Support the Equal Justice Initiative.")),a.a.createElement(s,{href:Object(i.a)("/introduction")},"Go to the documentation"))}},145:function(t,e,n){"use strict";var r=n(0),a=n(39);e.a=function(){return Object(r.useContext)(a.a)}},149:function(t,e,n){"use strict";n.d(e,"a",(function(){return a}));var r=n(145);function a(t){const{siteConfig:e}=Object(r.a)(),{baseUrl:n="/"}=e||{};if(!t)return t;return/^(https?:|\/\/)/.test(t)?t:t.startsWith("/")?n+t.slice(1):n+t}},156:function(t,e,n){"use strict";var r=n(19);n.d(e,"a",(function(){return r.c})),n.d(e,"b",(function(){return r.d})),n.d(e,"c",(function(){return r.e}))}}]);
