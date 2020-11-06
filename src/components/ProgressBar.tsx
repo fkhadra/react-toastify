5680979a0f945362c3a51e4feb525e0972f1c4a8
Moritz Gunz <moritz.gunz@gmail.com>
2020-11-06 08:53:45 +0100

    #447 replace classnames by clsx

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import cx from 'classnames';
+import cx from 'clsx';
 
 import { TYPE, DEFAULT, isFn } from './../utils';
 import { TypeOptions, ToastClassName } from '../types';
--- src/components/ToastContainer.tsx
+++ src/components/ToastContainer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
+import cx from 'clsx';
 
 import { Toast } from './Toast';
 import { CloseButton } from './CloseButton';
