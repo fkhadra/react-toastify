8fa150d8e19c55ce9550a44581c69aa5d76d21da
AmooHashem <45389673+AmooHashem@users.noreply.github.com>
2020-12-20 21:29:50 +0330

    Update collapseToast.ts

--- src/utils/collapseToast.ts
+++ src/utils/collapseToast.ts
@@ -20,7 +20,7 @@ export function collapseToast(
       style.height = '0';
       style.padding = '0';
       style.margin = '0';
-      setTimeout(() => done(), duration as number);
+//       setTimeout(() => done(), duration as number);
     });
   });
 }
