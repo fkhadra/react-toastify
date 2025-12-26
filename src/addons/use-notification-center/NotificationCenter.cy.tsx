696afc9275a8f425a22fe8069b0158208bc3338d
Moamen-w-a <moamencodegt@gmail.com>
2025-12-26 02:35:47 +0200

    Fix: Use relative import in useNotificationCenter for consistency
    - Changed import from 'react-toastify' to '../../index' in useNotificationCenter.ts
    - Updated test import to '../../../src' for consistency
    - Ensures build and tests work correctly

--- src/addons/use-notification-center/NotificationCenter.cy.tsx
+++ src/addons/use-notification-center/NotificationCenter.cy.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { toast, ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from '../../../src';
 import { NotificationCenterItem, useNotificationCenter, UseNotificationCenterParams } from './useNotificationCenter';
 
 function TestComponent(props: UseNotificationCenterParams) {
